f6320ad326e56403a380a24d3355c1fac3621a7d
katamus <christian.catamuscay.h@gmail.com>
2021-10-18 12:54:32 -0500

    77 y 78 paso de variables entre dos componentes a través de un servicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Articulo } from '../models/articulo';
+import { ArticulosService } from '../services/articulos.service';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  articulos: Array<Articulo> = new Array<Articulo>();
+  constructor(public usuarioInyectado:UsuarioService,public ArticuloInyectado:ArticulosService, private Ruta: Router ) {  }
 
   ngOnInit(): void {
+    this.articulos.push({
+      titulo:'Curso de Net Core',
+      descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum tempus metus nec rutrum. Vivamus non luctus dolor, vel tristique massa. Nam iaculis mauris tortor, ut facilisis sem aliquet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo condimentum diam at blandit. Morbi non nulla est. Quisque aliquet lacus at sem rhoncus pretium. Morbi posuere felis at ligula placerat tincidunt. Aliquam vel molestie nunc, eu dignissim lacus.',
+      fecha: new Date(),
+      usuario: ` Hola: ${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido} `
+      },{
+        titulo:'Curso de Angular',
+        descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum tempus metus nec rutrum. Vivamus non luctus dolor, vel tristique massa. Nam iaculis mauris tortor, ut facilisis sem aliquet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo condimentum diam at blandit. Morbi non nulla est. Quisque aliquet lacus at sem rhoncus pretium. Morbi posuere felis at ligula placerat tincidunt. Aliquam vel molestie nunc, eu dignissim lacus.',
+        fecha: new Date('04/25/2019'),
+        usuario: ` Hola: ${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido} `
+      },{
+        titulo:'Curso de Flutter',
+        descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum tempus metus nec rutrum. Vivamus non luctus dolor, vel tristique massa. Nam iaculis mauris tortor, ut facilisis sem aliquet eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo condimentum diam at blandit. Morbi non nulla est. Quisque aliquet lacus at sem rhoncus pretium. Morbi posuere felis at ligula placerat tincidunt. Aliquam vel molestie nunc, eu dignissim lacus.',
+        fecha: new Date('05/10/2019'),
+        usuario: ` Hola: ${this.usuarioInyectado.usuario.nombre} ${this.usuarioInyectado.usuario.apellido} `
+      }
+    )
+  }
+
+  irAlDetalle(articulo:Articulo){
+    this.ArticuloInyectado.articulo = articulo;
+    this.Ruta.navigateByUrl('/articulo-detalle');
+
   }
 
 }
---
+++ src/app/models/articulo.ts
@@ -0,0 +1,13 @@
+export class Articulo{
+    titulo!:string;
+    descripcion!:string;
+    usuario!:string;
+    fecha!: Date;
+
+    constructor(){
+        this.titulo = this.titulo;
+        this.descripcion = this.descripcion;
+        this.usuario = this.usuario;
+        this.fecha = this.fecha;
+    }
+}
